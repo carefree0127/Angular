e4b3855b35dae7b13782c6c28a83c332a2de0acd
Ruby <carefree0127@hotmail.com>
2020-02-09 13:43:11 +0800

    [src]="sitelogo"

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   sitename = 'Hello World';
-
+  sitelogo = '/assets/images/logo.png';
   constructor() { }
 
   ngOnInit(): void {
